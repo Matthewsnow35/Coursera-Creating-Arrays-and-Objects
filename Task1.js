2a0d0578cb96cb85d76b74d19d5da7508637bcf3
Matthew Snow <68118415+Matthewsnow35@users.noreply.github.com>
2024-04-03 17:23:58 -0400

    Added Coursera Activity Instructions
    Coursera questions and corresponding answers.

---
+++ Task1.js
@@ -0,0 +1,41 @@
+/* 
+In this exercise lab you will practice creating arrays and objects.
+
+Tasks to complete
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object.
+
+Solution:
+*/
+var clothes = []
+clothes.push ('Pants')
+clothes.push ('Socks')
+clothes.push ('Long Shirts')
+clothes.push ('Diamond Chain')
+clothes.push ('Scarf')
+
+clothes.pop()
+
+clothes.push('Hat')
+
+console.log(clothes[2]);
+
+var favCar = {
+    convertible: true,
+}
+console.log(favCar);
